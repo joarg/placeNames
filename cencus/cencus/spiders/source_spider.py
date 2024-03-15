d5794bae4f694023724af90365f2c51a2228a069
joar <joar.glosvik@gmail.com>
2024-03-15 20:33:49 +0000

    scrapy_cloud got these

--- cencus/cencus/spiders/source_spider.py
+++ cencus/cencus/spiders/source_spider.py
@@ -14,8 +14,8 @@ class SourceSpider(scrapy.Spider):
         with open('data/sources.json', 'r') as f:
             sources = json.load(f)
             # random_sources = random.sample(sources, 2)
-        fromSource = 10
-        toSource = 100
+        fromSource = 200
+        toSource = 300
 
         for source in sources[fromSource:toSource]:
             source_id = source['id']
